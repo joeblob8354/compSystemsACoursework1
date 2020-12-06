1047ff0e8dab21df8d38fad609133313b55af7b3
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-06 16:10:44 +0000

    changed ip to localhost and added 's' key functionality.

--- distributed/controller/gol/gol.go
+++ distributed/controller/gol/gol.go
@@ -89,8 +89,11 @@ func engine(p Params, d distributorChannels, k <-chan rune) {
     for turn = 0; turn < p.Turns; turn++ {
         client.Call("Engine.Run", data, &reply)
         data.World = reply
-        if <- k == 's' {
-            outputPgmFile(d, p, data.World, turn)
+        select {
+            case key := <- k:
+                if key == 's' {
+                    outputPgmFile(d, p, data.World, turn)
+                }
         }
     }
 
