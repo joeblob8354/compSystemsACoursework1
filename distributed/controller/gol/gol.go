7e1b898d489f3d1e3ff9ee321a8f08e82503e94c
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-06 15:51:23 +0000

    changed ip to localhost and added 's' key functionality.

--- distributed/controller/gol/gol.go
+++ distributed/controller/gol/gol.go
@@ -44,13 +44,14 @@ func calculateAliveCells(p Params, world [][]byte) []util.Cell {
 	return aliveCells
 }
 
-func engine(p Params, d distributorChannels) {
+func engine(p Params, d distributorChannels, k keyPresses) {
 
     //Creates a 2D slice to store the world.
     newWorld := make([][]byte, p.ImageHeight)
     for i := 0; i < p.ImageHeight; i++ {
         newWorld[i] = make([]byte, p.ImageWidth)
     }
+
     //sends command to io so it executes the readPgmImage() function
     d.ioCommand <- 1
     //sends created filename to readPgmImage()
@@ -64,58 +65,62 @@ func engine(p Params, d distributorChannels) {
     }
 
     //connect to server or return an error
-    serverAddress := "18.232.162.211:8030"
+    serverAddress := "localhost:8030"
     client, err := rpc.Dial("tcp", serverAddress)
 
     if err != nil {
         log.Fatal("connection error", err)
     }
 
+    //create var of type Data to store necessary data to send to logic engine.
     var data Data
     data.World = newWorld
     data.TheParams = p
     turn := 0
 
+    ///create a reply variable to receive the updated world from the logic engine
     var reply [][]byte
+
+    //create a new ticker and start it.
     tk := time.NewTicker(time.Second*1)
     go ticker(tk, &data.World, &turn, d, p)
+
     //call the Run method on the server and send it the world
     for turn = 0; turn < p.Turns; turn++ {
         client.Call("Engine.Run", data, &reply)
         data.World = reply
+        key := <- keyPresses
+        if key == 's' {
+            outputPgmFile(d, p, data.World)
+        }
     }
 
+    //stops ticker
     tk.Stop()
 
     //send array of alive cells for testing
     aliveCells := calculateAliveCells(p, data.World)
     d.events <- FinalTurnComplete{CompletedTurns: p.Turns, Alive: aliveCells}
 
-    //send command to io to let make it execute the writePgmImage() function.
-    d.ioCommand <- 0
-    //send the filename to the writePgmImage() function.
-    d.ioFilename <- (strconv.Itoa(p.ImageHeight) + "x" + strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(turn))
-
-    //Scan across the updated world and send bytes 1 at a time to the writePgmImage() function via the ioOutput channel.
-    for currRow := 0; currRow < p.ImageHeight; currRow++ {
-        for currColumn := 0; currColumn < p.ImageWidth; currColumn++ {
-            d.ioOutput <- data.World[currRow][currColumn]
-        }
-    }
+    //outputs pgm file
+    outputPgmFile(d, p, data.World)
 
     // Make sure that the Io has finished any output before exiting.
  	d.ioCommand <- ioCheckIdle
  	<-d.ioIdle
 
+    //close events channel
     close(d.events)
 }
 
+//ticker function that loops every 2 seconds and sends AliveCellsCount events
 func ticker(tk *time.Ticker, world *[][]byte, turn *int, d distributorChannels, p Params) {
     for range tk.C{
         d.events <- AliveCellsCount{CompletedTurns: *turn, CellsCount: checkNumberOfAliveCells(p, *world)}
     }
 }
 
+//returns number of alive cells in a world state
 func checkNumberOfAliveCells(p Params, world [][]byte) int {
 
     numberOfAliveCells := 0
@@ -129,7 +134,23 @@ func checkNumberOfAliveCells(p Params, world [][]byte) int {
 	return numberOfAliveCells
 }
 
-// Connect to server and run the Run method on there.
+//Outputs a program file of the world state.
+func outputPgmFile (d distributorChannels, p Params, world [][]byte) {
+
+    //send command to io to let make it execute the writePgmImage() function.
+    d.ioCommand <- 0
+    //send the filename to the writePgmImage() function.
+    d.ioFilename <- (strconv.Itoa(p.ImageHeight) + "x" + strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(turn))
+
+    //Scan across the updated world and send bytes 1 at a time to the writePgmImage() function via the ioOutput channel.
+    for currRow := 0; currRow < p.ImageHeight; currRow++ {
+        for currColumn := 0; currColumn < p.ImageWidth; currColumn++ {
+            d.ioOutput <- world[currRow][currColumn]
+        }
+    }
+}
+
+//create necessary channels and start go routines.
 func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
     //creates all necessary channels
@@ -148,7 +169,7 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
     	ioOutput,
     }
 
-    go engine(p, distributorChannels)
+    go engine(p, distributorChannels, keyPresses)
 
     ioChannels := ioChannels{
         command:  ioCommand,
