07f58237a9e991261d16579ac609b01aa5dbe560
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-08 18:30:56 +0000

    working on distributed aws split nodes

--- distributed/logicEngine/main.go
+++ distributed/logicEngine/main.go
@@ -46,7 +46,7 @@ func (e *Engine) RunMaster(data Data, reply *[][]byte) error {
 
     numberOfNodes := data.TheParams.Threads
     if numberOfNodes > len(nodeAddresses) {
-        fmt.Println("Not enough nodes available! Using ", len(nodeAddresses), " nodes instead.")
+        fmt.Println("Not enough nodes available! Using", len(nodeAddresses), "nodes instead.")
         numberOfNodes = len(nodeAddresses)
     }
 
