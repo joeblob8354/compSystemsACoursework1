f4f4fc5668e35ee799f6d7adb25e0baa545a17ec
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-09 21:55:40 +0000

    working on benchmarks for parallel

--- parallel/bench_test.go
+++ parallel/bench_test.go
@@ -7,9 +7,9 @@ import (
 var p gol.Params
 
 func benchmarkParallel(p gol.Params, b *testing.B) {
-    p.ImageHeight = 128
-    p.ImageWidth = 128
-    p.Turns = 100000
+    p.ImageHeight = 256
+    p.ImageWidth = 256
+    p.Turns = 100
     for n := 0; n < b.N; n++ {
         events := make(chan gol.Event)
         gol.Run(p, events, nil)
@@ -36,71 +36,71 @@ func BenchmarkParallel2(b *testing.B) {
 }
 
 func BenchmarkParallel3(b *testing.B) {
-    p.Threads = 3
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel4(b *testing.B) {
     p.Threads = 4
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel5(b *testing.B) {
-    p.Threads = 5
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel6(b *testing.B) {
+func BenchmarkParallel4(b *testing.B) {
     p.Threads = 6
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel7(b *testing.B) {
-    p.Threads = 7
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel8(b *testing.B) {
+func BenchmarkParallel5(b *testing.B) {
     p.Threads = 8
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel9(b *testing.B) {
-    p.Threads = 9
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel10(b *testing.B) {
+func BenchmarkParallel6(b *testing.B) {
     p.Threads = 10
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel11(b *testing.B) {
-    p.Threads = 11
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel12(b *testing.B) {
+func BenchmarkParallel7(b *testing.B) {
     p.Threads = 12
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel13(b *testing.B) {
-    p.Threads = 13
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel14(b *testing.B) {
+func BenchmarkParallel8(b *testing.B) {
     p.Threads = 14
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel15(b *testing.B) {
-    p.Threads = 15
+func BenchmarkParallel9(b *testing.B) {
+    p.Threads = 16
     benchmarkParallel(p, b)
 }
 
-func BenchmarkParallel16(b *testing.B) {
-    p.Threads = 16
+func BenchmarkParallel10(b *testing.B) {
+    p.Threads = 18
+    benchmarkParallel(p, b)
+}
+
+func BenchmarkParallel11(b *testing.B) {
+    p.Threads = 20
+    benchmarkParallel(p, b)
+}
+
+func BenchmarkParallel12(b *testing.B) {
+    p.Threads = 22
+    benchmarkParallel(p, b)
+}
+
+func BenchmarkParallel13(b *testing.B) {
+    p.Threads = 24
+    benchmarkParallel(p, b)
+}
+
+func BenchmarkParallel14(b *testing.B) {
+    p.Threads = 26
+    benchmarkParallel(p, b)
+}
+
+func BenchmarkParallel28(b *testing.B) {
+    p.Threads = 28
+    benchmarkParallel(p, b)
+}
+
+func BenchmarkParallel30(b *testing.B) {
+    p.Threads = 30
     benchmarkParallel(p, b)
 }
