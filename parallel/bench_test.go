c09f2159788b8744a14f99654249295e6df56c4f
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-09 21:35:28 +0000

    working on benchmarks for parallel

--- parallel/bench_test.go
+++ parallel/bench_test.go
@@ -9,7 +9,7 @@ var p gol.Params
 func benchmarkParallel(p gol.Params, b *testing.B) {
     p.ImageHeight = 128
     p.ImageWidth = 128
-    p.Turns = 10000
+    p.Turns = 100000
     for n := 0; n < b.N; n++ {
         events := make(chan gol.Event)
         gol.Run(p, events, nil)
