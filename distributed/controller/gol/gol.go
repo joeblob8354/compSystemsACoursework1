6c22f847d91a5067bf8ff9446289c1a0b21bf6d8
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-09 00:45:21 +0000

    added 'k' functionality

--- distributed/controller/gol/gol.go
+++ distributed/controller/gol/gol.go
@@ -150,7 +150,8 @@ func engine(p Params, d distributorChannels, k <-chan rune) {
             case key = <- k:
                 //if s is pressed output a pgm img of the current world state and the corresponding turn.
                 if key == 's' {
-                    verfy := outputPgmFile(d, p, data.World, turn)
+                    verify := outputPgmFile(d, p, data.World, turn)
+                    verify = false
 
                 //if p is pressed, change state to paused, stop the ticker, and wait for p to be pressed again before continuing.
                 } else if key == 'p' {
@@ -191,6 +192,7 @@ func engine(p Params, d distributorChannels, k <-chan rune) {
 
     //outputs pgm file
     verify := outputPgmFile(d, p, data.World, turn)
+    verify = false
 
     // Make sure that the Io has finished any output before exiting.
  	d.ioCommand <- ioCheckIdle
