d595007ef7127306c9e19b7b186f4beadd567346
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-10 05:35:01 +0000

    working on benchmarks for parallel

--- distributed/controller/bench_test.go
+++ distributed/controller/bench_test.go
@@ -9,7 +9,7 @@ var p gol.Params
 func benchmarkParallel(p gol.Params, b *testing.B) {
     p.ImageHeight = 512
     p.ImageWidth = 512
-    p.Turns = 5000
+    p.Turns = 100
     for n := 0; n < b.N; n++ {
         events := make(chan gol.Event)
         gol.Run(p, events, nil)
@@ -36,71 +36,71 @@ func BenchmarkParallel2(b *testing.B) {
 }
 
 func BenchmarkParallel3(b *testing.B) {
-    p.Threads = 4
+    p.Threads = 3
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel4(b *testing.B) {
-    p.Threads = 6
+    p.Threads = 4
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel5(b *testing.B) {
-    p.Threads = 8
+    p.Threads = 5
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel6(b *testing.B) {
-    p.Threads = 10
+    p.Threads = 6
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel7(b *testing.B) {
-    p.Threads = 12
+    p.Threads = 7
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel8(b *testing.B) {
-    p.Threads = 14
+    p.Threads = 8
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel9(b *testing.B) {
-    p.Threads = 16
+    p.Threads = 9
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel10(b *testing.B) {
-    p.Threads = 18
+    p.Threads = 10
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel11(b *testing.B) {
-    p.Threads = 20
+    p.Threads = 11
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel12(b *testing.B) {
-    p.Threads = 22
+    p.Threads = 12
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel13(b *testing.B) {
-    p.Threads = 24
+    p.Threads = 13
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel14(b *testing.B) {
-    p.Threads = 26
+    p.Threads = 14
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel28(b *testing.B) {
-    p.Threads = 28
+    p.Threads = 15
     benchmarkParallel(p, b)
 }
 
 func BenchmarkParallel30(b *testing.B) {
-    p.Threads = 30
+    p.Threads = 16
     benchmarkParallel(p, b)
 }
