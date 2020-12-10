4cc16a8420d0b44fffd1fece405ee09a915b24f6
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-10 17:03:56 +0000

    changing ips for aws nodes

--- distributed/controller/bench_test.go
+++ distributed/controller/bench_test.go
@@ -43,18 +43,18 @@ func BenchmarkParallel3(b *testing.B) {
 func BenchmarkParallel4(b *testing.B) {
     p.Threads = 4
     benchmarkParallel(p, b)
-}*/
+}
 
 func BenchmarkParallel5(b *testing.B) {
     p.Threads = 5
     benchmarkParallel(p, b)
-}
-/*
+}*/
+
 func BenchmarkParallel6(b *testing.B) {
     p.Threads = 6
     benchmarkParallel(p, b)
 }
-
+/*
 func BenchmarkParallel7(b *testing.B) {
     p.Threads = 7
     benchmarkParallel(p, b)
