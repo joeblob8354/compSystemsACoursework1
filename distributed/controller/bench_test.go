a4195adc0b750aaff6e887065933685ea2d5ff7e
joeblob8354 <kp18724@bristol.ac.uk>
2020-12-11 00:41:36 +0000

    changing ips for aws nodes

--- distributed/controller/bench_test.go
+++ distributed/controller/bench_test.go
@@ -6,10 +6,10 @@ import (
         )
 var p gol.Params
 
-func benchmarkParallel(p gol.Params, b *testing.B) {
+func benchmarkDistributed(p gol.Params, b *testing.B) {
     p.ImageHeight = 16
     p.ImageWidth = 16
-    p.Turns = 250
+    p.Turns = 500
     for n := 0; n < b.N; n++ {
         events := make(chan gol.Event)
         gol.Run(p, events, nil)
@@ -24,83 +24,83 @@ func benchmarkParallel(p gol.Params, b *testing.B) {
         }
     }
 }
-/*
-func BenchmarkParallel1(b *testing.B) {
+
+func BenchmarkDistributed1(b *testing.B) {
     p.Threads = 1
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel2(b *testing.B) {
+func BenchmarkDistributed2(b *testing.B) {
     p.Threads = 2
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel3(b *testing.B) {
+func BenchmarkDistributed3(b *testing.B) {
     p.Threads = 3
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel4(b *testing.B) {
-    p.Threads = 4
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel5(b *testing.B) {
-    p.Threads = 5
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel6(b *testing.B) {
-    p.Threads = 6
-    benchmarkParallel(p, b)
-}
-*/
-func BenchmarkParallel7(b *testing.B) {
-    p.Threads = 7
-    benchmarkParallel(p, b)
-}
-
-func BenchmarkParallel8(b *testing.B) {
-    p.Threads = 8
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }/*
 
-func BenchmarkParallel9(b *testing.B) {
+func BenchmarkDistributed4(b *testing.B) {
+    p.Threads = 4
+    benchmarkDistributed(p, b)
+}
+
+func BenchmarkDistributed5(b *testing.B) {
+    p.Threads = 5
+    benchmarkDistributed(p, b)
+}
+
+func BenchmarkDistributed6(b *testing.B) {
+    p.Threads = 6
+    benchmarkDistributed(p, b)
+}
+
+func BenchmarkDistributed7(b *testing.B) {
+    p.Threads = 7
+    benchmarkDistributed(p, b)
+}
+
+func BenchmarkDistributed8(b *testing.B) {
+    p.Threads = 8
+    benchmarkDistributed(p, b)
+}
+
+func BenchmarkDistributed9(b *testing.B) {
     p.Threads = 9
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel10(b *testing.B) {
+func BenchmarkDistributed10(b *testing.B) {
     p.Threads = 10
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel11(b *testing.B) {
+func BenchmarkDistributed11(b *testing.B) {
     p.Threads = 11
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel12(b *testing.B) {
+func BenchmarkDistributed12(b *testing.B) {
     p.Threads = 12
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel13(b *testing.B) {
+func BenchmarkDistributed13(b *testing.B) {
     p.Threads = 13
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel14(b *testing.B) {
+func BenchmarkDistributed14(b *testing.B) {
     p.Threads = 14
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel28(b *testing.B) {
+func BenchmarkDistributed28(b *testing.B) {
     p.Threads = 15
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }
 
-func BenchmarkParallel30(b *testing.B) {
+func BenchmarkDistributed30(b *testing.B) {
     p.Threads = 16
-    benchmarkParallel(p, b)
+    benchmarkDistributed(p, b)
 }*/
